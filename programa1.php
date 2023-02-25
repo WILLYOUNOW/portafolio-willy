a1776001b72a764a692023012190580971c4c6a3
WILLYOUNOW <114510215+WILLYOUNOW@users.noreply.github.com>
2023-02-25 22:44:28 +0000

    Initial commit

---
+++ programa1.php
@@ -0,0 +1,6 @@
+<?PHP
+echo "Mi primer programa en PHP";
+echo "<br>";
+echo "<br>";
+echo "Carlos Gustavo Willy Lozada";
+?>
---
+++ programa2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Programa 2  </title>
+</head>
+<body>
+<?PHP
+echo "Si lo puedes soñar";
+echo "<br>";
+echo " Lo puedes hacer";
+echo "<br>";
+//CONCATENACION
+$var1="Carlos";
+$var2="Willy";
+echo $var1." ".$var2;
+?>
+<./body>
+<./html>
